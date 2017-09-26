b42ac474cfc4916800b24d15dc566aaa2f429b04
Linus Heckemann <git@sphalerite.org>
2017-09-26 14:51:06 +0100

    Add kakuna

---
+++ servers/kakuna.nix
@@ -0,0 +1,24 @@
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [
+      ../server.nix
+      ../ldap.nix
+    ];
+
+  boot.loader.grub.enable = true;
+  boot.loader.grub.version = 2;
+  boot.loader.grub.device = "/dev/sda";
+
+  networking = {
+    hostName = "kakuna";
+    interfaces.enp4s0.ip4 = [ { address = "130.159.141.112"; prefixLength = 26;} ];
+  };
+
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+
+  # The NixOS release to be compatible with for stateful data such as databases.
+  system.stateVersion = "17.09";
+}
