1fc0366dcf7bf873e9608e0434c07f565e3a1704
Linus Heckemann <git@sphalerite.org>
2017-09-26 14:31:58 +0100

    Add sudo for SysAdminWG

--- ldap.nix
+++ ldap.nix
@@ -7,8 +7,15 @@
     nsswitch = true;
     loginPam = true;    
   };
+
   security.pam.services.ldap.makeHomeDir = true;
+
+  # So that the LDAP shell entries can be found
   system.activationScripts.linkShells = ''
     ln -sf ${pkgs.bashInteractive}/bin/bash /bin/bash
+  '';
+
+  security.sudo.extraConfig = ''
+    %SysAdminWG      ALL=(ALL:ALL) SETENV: ALL
   '';
 }
