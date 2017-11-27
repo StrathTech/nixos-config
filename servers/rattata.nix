10ea90b7117f964fe93f0fc9e6d00d648a05a2bd
Linus Heckemann <git@sphalerite.org>
2017-11-27 23:28:45 +0000

    Add rattata

---
+++ servers/rattata.nix
@@ -0,0 +1,23 @@
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [
+      ../server.nix
+      ../ldap.nix
+      ../homedirs.nix
+      ../binary-cache.nix
+    ];
+
+  boot.loader.grub.enable = true;
+  boot.loader.grub.version = 2;
+  boot.loader.grub.device = "/dev/sda";
+
+  networking = {
+    hostName = "rattata.strathtech.co.uk";
+    interfaces.enp4s0.ip4 = [ { address = "130.159.141.75"; prefixLength = 26;} ];
+  };
+
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+}
