83ef0a50c2a9659295d29db9dcf54fb3f4b81971
Linus Heckemann <git@sphalerite.org>
2018-05-03 16:54:24 +0100

    add mewtwo

---
+++ servers/mewtwo.nix
@@ -0,0 +1,27 @@
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [
+      ../server.nix
+      ../ldap.nix
+    ];
+
+  boot.loader.grub.enable = true;
+  boot.loader.grub.device = "/dev/sda";
+  boot.supportedFileSystems = [ "zfs" ];
+
+  networking = {
+    hostId = "6d657774";
+    hostName = "mewtwo";
+    interfaces.ext.ip4 = [ { address = "130.159.141.100"; prefixLength = 26;} ];
+  };
+
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+
+  services.udev.extraRules = ''
+    SUBSYSTEM=="net", ACTION=="add", DRIVERS=="?*", ATTR{address}=="00:30:05:fc:5e:62", ATTR{dev_id}=="0x0", ATTR{type}=="1", KERNEL=="eth*", NAME="ext"
+  '';
+  boot.loader.grub.useOSProber = true;
+}
