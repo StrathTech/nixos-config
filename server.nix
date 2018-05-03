0175aa2bcc83924a4c20b730a7f4f5ca31b323f8
Linus Heckemann <git@sphalerite.org>
2018-05-03 16:50:27 +0100

    servers: disable graphical grub

--- server.nix
+++ server.nix
@@ -4,4 +4,6 @@
   networking.defaultGateway = {address = "130.159.141.126"; interface = "enp4s0";};
   networking.domain = "strathtech.co.uk";
   networking.nameservers = [ "130.159.141.73" "130.159.141.74" ];
+
+  boot.loader.grub.splashImage = null;
 }
